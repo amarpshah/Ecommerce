8b2ae616c8b9015f337995a66ac8e7ec3175ca8c
amarpshah <amar.p.shah@gmail.com>
2025-07-25 15:26:10 +0530

    Adding the Basket project

---
+++ Ecommerce/Services/Basket/Basket.Core/Entities/BasketCheckout.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.Core.Entities
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public string AddressLine { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public string Cvv { get; set; }
+    }
+}
