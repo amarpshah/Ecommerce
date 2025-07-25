4b25ed810d049203d81dd1a8259bcf7138759d88
amarpshah <amar.p.shah@gmail.com>
2025-07-25 21:07:17 +0530

    Changes in the basket microservice and related docker changes

---
+++ Ecommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    public class BasketController : ApiController
+    {
+        public IMediator Mediator { get; }
+
+        public BasketController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+        public async Task< ActionResult<ShoppingCartResponse>> GetBasketByUserName(string userName)
+        { 
+            var query = new GetBasketByUserNameQuery(userName);
+            var basket = await Mediator.Send(query);
+            return Ok(basket);
+        }
+
+        [HttpPost("CreateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand createShoppingCartCommand )
+        {
+            var basket = await Mediator.Send(createShoppingCartCommand);        
+            return Ok(basket);
+        }
+
+        [HttpDelete]
+        [Route("[action]/{userName}", Name = "DeleteBasketByUserName")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteBasketByUserName(string userName)
+        { 
+            var cmd = new DeleteBasketByUserNameCommand(userName);
+            await Mediator.Send(cmd);
+            return Ok();
+        }
+    }
+}
--- Ecommerce/Services/Basket/Basket.API/Program.cs
+++ Ecommerce/Services/Basket/Basket.API/Program.cs
@@ -1,17 +1,49 @@
+using Basket.Application.Handlers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(option =>
+{
+    option.ReportApiVersions = true;
+    option.AssumeDefaultVersionWhenUnspecified = true;
+    option.DefaultApiVersion = new Asp.Versioning.ApiVersion(1.0);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", 
+    new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Basket.API", 
+        Description = "This API will interact with the Basket API for the ecommerce application", 
+        Version = "v1" }));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var assemblies = new Assembly[] {
+    Assembly.GetExecutingAssembly(),
+    typeof(GetBasketByUserNameHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+//Redis
+builder.Services.AddStackExchangeRedisCache(options => 
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
